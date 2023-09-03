627d3b3c56eea4baee3098c7df16ef24391a1835
André van Delft <andrevandelft@outlook.com>
2023-09-03 13:21:50 +0200

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py compress
-gunicorn heidegger_translations.wsgi:application --bind 0.0.0.0:$1
+gunicorn heidegger_landing.wsgi:application --bind 0.0.0.0:$1
